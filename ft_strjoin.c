db72a1c90c897443a4e03f82664ad7e57d37f288
edvin3i <edviniii@gmail.com>
2021-10-24 16:20:27 +0300

    ft_strjoin works and tested

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,7 +12,23 @@
 
 #include "libft.h"
 
-char 	*ft_strjoin(char const *s1, char const *s2)
+char 	*ft_strjoin(char const *str1, char const *str2)
 {
-	return (NULL);
+	char *result;
+	char *res_iter;
+	int	res_len;
+
+	if (!str1 || !str2)
+		return (NULL);
+	res_len = ft_strlen(str1) + ft_strlen(str2) + 1;
+	result = (char *)malloc(res_len * sizeof(char));
+	if (!result)
+		return (NULL);
+	res_iter = result;
+	while (*str1)
+		*res_iter++ = *str1++;
+	while (*str2)
+		*res_iter++ = *str2++;
+	*res_iter = 0x00;
+	return (result);
 }
