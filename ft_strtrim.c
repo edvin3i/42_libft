8f8459cf05639ca73b6635b45add6308500d9984
edvin3i <edviniii@gmail.com>
2021-10-25 00:28:02 +0300

    ft_strtrim is worked and tested

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,7 +12,23 @@
 
 #include "libft.h"
 
-char 	*ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	return (NULL);
+	size_t	start;
+	size_t	end;
+	char	*result;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (s1[end] && ft_strchr(set, s1[end]) && start < end)
+		end--;
+	result = (char *)malloc(sizeof(char) * (end - start + 2));
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, &s1[start], end - start + 2);
+	return (result);
 }
