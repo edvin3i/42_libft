b2074eefaa0e6ec5ae04e5c73156d6f30ce8a057
edvin3i <edviniii@gmail.com>
2021-10-28 01:04:38 +0300

    ft_striteri is worked and tested

--- ft_striteri.c
+++ ft_striteri.c
@@ -14,5 +14,14 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int	index;
 
+	if (!s || !f)
+		return ;
+	index = 0;
+	while (s[index])
+	{
+		f(index, &s[index]);
+		index++;
+	}
 }
