1f6b8246118e68fb62d47c3295ffef92add81990
edvin3i <edviniii@gmail.com>
2021-11-09 15:52:06 +0500

    ft_lstclear is done

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbreana <gbreana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/21 09:06:14 by gbreana           #+#    #+#             */
+/*   Updated: 2020/08/21 10:54:19 by gbreana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*slst;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		slst = *lst;
+		ft_lstdelone(slst, del);
+		*lst = (*lst)->next;
+	}
+}
